892ec93e325ba2d387d5d77f075b2169cd0b57ef
Matthieu Mota <matthieumota@gmail.com>
2018-05-16 17:02:05 +0200

    Add router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -8,6 +9,8 @@ import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { CounterComponent } from './counter/counter.component';
 import { DropdownToggleDirective } from './directives/dropdown-toggle.directive';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -16,10 +19,17 @@ registerLocaleData(localeFr, 'fr');
     AppComponent,
     MenuComponent,
     CounterComponent,
-    DropdownToggleDirective
+    DropdownToggleDirective,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Grâce au RouterModule, on définit toutes les routes de notre application
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'register', component: RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
